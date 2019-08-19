826910dabe8979fb820050e23147136c312f3790
Igor Kwiatkowski <waffel@gmail.com>
2019-08-19 13:25:47 +0200

    created initial models

--- donate/models.py
+++ donate/models.py
@@ -6,3 +6,18 @@ class User(AbstractUser):
     pass
 
 
+class Organization(models.Model):
+    name = models.CharField(max_length=128)
+
+
+class ProductType(models.Model):
+    name = models.CharField(max_length=128)
+
+
+class Donation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    beneficiary = models.ForeignKey(Organization, on_delete=models.SET_NULL, null=True)
+    content = models.ManyToManyField(ProductType)
+    number_of_bags = models.IntegerField()
+    pickup_address = models.CharField(max_length=128)
+    complete = models.BooleanField(default=False)
