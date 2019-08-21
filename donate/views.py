95aaa578baec365ef0d0e07848ac5956140e27c5
Igor Kwiatkowski <waffel@gmail.com>
2019-08-21 13:57:46 +0200

    login.html now uses django form, looks terrible

--- donate/views.py
+++ donate/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views import View
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from .forms import LoginForm
 
-# Create your views here.
+
+class LoginView(View):
+    def get(self, request):
+        form = LoginForm()
+        return render(request, 'login.html', {'form': form})
+
+    def post(self, request):
+        email = request.POST['email']
+        password = request.POST['password']
+        user = authenticate(username=email, password=password)
+        if user is not None:
+            login(request, user)
