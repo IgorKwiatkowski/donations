d980f6f2d82a48ca93ef323582a498d5137884c7
Igor Kwiatkowski <waffel@gmail.com>
2019-08-22 15:15:35 +0200

    added Organization to admin panel

--- donate/admin.py
+++ donate/admin.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 
 from .forms import CustomUserCreationForm, CustomUserChangeForm
-from .models import User
+from .models import User, Organization
 
 
 class CustomUserAdmin(UserAdmin):
@@ -14,3 +14,5 @@ class CustomUserAdmin(UserAdmin):
 
 
 admin.site.register(User, CustomUserAdmin)
+admin.site.register(Organization)
+
